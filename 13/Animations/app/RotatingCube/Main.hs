f4feaba2750eaac1bb3cbf5a490656f7983c33cd
pawelfranczak <pawel.fran@gmail.com>
2024-11-01 11:53:39 +0100

    new animation

--- 13/Animations/app/RotatingCube/Main.hs
+++ 13/Animations/app/RotatingCube/Main.hs
@@ -7,7 +7,7 @@ rotatingCube :: Float -> VisObject Float
 rotatingCube t = RotEulerRad (Euler 0 0 t) (Cube 1 Solid blue)
 
 orient :: VisObject Float -> VisObject Float
-orient pict = RotEulerDeg (Euler 270 180 0) $ pict
+orient = RotEulerDeg (Euler 270 180 0) 
 
 main :: IO ()
 main = animate defaultOpts (orient . rotatingCube)
